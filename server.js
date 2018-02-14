710bd7b297786656532174f648d1b8f821d6d9b6
Juan Pedro Danculovic <jdancu@MacBook-Pro-de-Juan.local>
2018-02-13 22:17:19 -0300

    heroku bugfixes

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
